3c6be1cb007aac4a241f84a80b944c04468f2181
iamfrego <mate_freg@tiscali.it>
2021-10-19 14:50:17 +0200

    Palindromi

---
+++ Palindromia/assets/js/main.js
@@ -0,0 +1,22 @@
+const check = document.querySelector("div.palindromo")
+
+let inputWorld = prompt("Inserisci una parola");
+let reverseWorld = invertWorld(inputWorld);
+
+if (inputWorld == reverseWorld) {
+    check.innerHTML = "La parola è palindroma";
+}
+
+else {
+    check.innerHTML = "La parola non è palindroma";
+}
+function invertWorld(parola) {
+    let temp = "";
+    for (let i = parola.length - 1; i >= 0; i--) {
+        temp += parola[i]
+    }
+    return temp;
+}
+
+
+
